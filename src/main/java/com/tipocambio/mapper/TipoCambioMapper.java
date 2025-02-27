b08cf37d7eccf289fee16a650568034ce0aaa264
dhuaman9 <dhuaman9@outlook.com>
2025-02-27 10:38:36 -0500

    eliminando comentariosss

--- src/main/java/com/tipocambio/mapper/TipoCambioMapper.java
+++ src/main/java/com/tipocambio/mapper/TipoCambioMapper.java
@@ -10,7 +10,7 @@ import com.tipocambio.dto.TipoCambioRequestDTO;
 import com.tipocambio.dto.TipoCambioResponseDTO;
 import com.tipocambio.model.TipoCambio;
 
-//@Mapper(componentModel = "spring")
+
 @Component
 public class  TipoCambioMapper {
 	
@@ -47,7 +47,6 @@ public class  TipoCambioMapper {
     // Convierte de CalcularTipoCambioRequestDTO  a CalcularTipoCambioResponseDTO
     public CalcularTipoCambioResponseDTO toResponseDTO(CalcularTipoCambioRequestDTO dto, TipoCambio tipoCambio, BigDecimal montoConTipoCambio) {
     	
-       // BigDecimal montoConTipoCambio = dto.getMonto().multiply(tipoCambio.getTipoCambio());
         return CalcularTipoCambioResponseDTO.builder()
                 .monto(dto.getMonto())
                 .montoConTipoCambio(montoConTipoCambio)
